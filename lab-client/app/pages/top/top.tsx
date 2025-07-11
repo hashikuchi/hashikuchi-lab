fcb94435f6381aa8ed9e1873e53c4725330ff198
hashikuchi <k.hashikuchi@gmail.com>
2025-07-11 14:50:27 +0900

    add mantine

---
+++ lab-client/app/pages/top/top.tsx
@@ -0,0 +1,31 @@
+import { AppShell, Box, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+export function Top() {
+    const [opened, { toggle }] = useDisclosure();
+    return (
+    <AppShell
+        header={{ height: 60 }}
+        navbar={{
+            width: 300,
+            breakpoint: 'sm', 
+            collapsed: { mobile: !opened },
+        }}
+        padding="md"
+        >
+        <AppShell.Header>
+            <Burger
+                opened={opened} 
+                onClick={toggle}
+                hiddenFrom="sm"
+                size="sm"
+            />
+            <Box p='md'>Hashikuchi's Lab</Box>
+        </AppShell.Header>
+
+        <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+        <AppShell.Main>Main</AppShell.Main>
+        </AppShell>
+    );
+}
