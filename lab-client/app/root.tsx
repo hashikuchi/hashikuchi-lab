b4e8c0bc281df6811ecab2b451c12179a5ca1c4e
hashikuchi <k.hashikuchi@gmail.com>
2025-07-23 19:27:32 +0900

    Fixed the mantine style

--- lab-client/app/root.tsx
+++ lab-client/app/root.tsx
@@ -8,9 +8,9 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import "./app.css";
+import { MantineProvider, ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
+// import "./app.css";
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -27,15 +27,16 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" {...mantineHtmlProps}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <ColorSchemeScript />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <MantineProvider>{children}</MantineProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -44,7 +45,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <MantineProvider><Outlet /></MantineProvider>;
+  return <Outlet />;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
