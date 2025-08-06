dfdd5d5376667a302bd54c879e970265c4e9e2e9
hashikuchi <k.hashikuchi@gmail.com>
2025-08-06 15:30:17 +0900

    add anew page to demonstrate a symple state management

--- lab-client/app/routes/home.tsx
+++ lab-client/app/routes/home.tsx
@@ -1,12 +1,5 @@
 import { Top } from "../pages/top";
 
-export function meta() {
-  return [
-    { title: "Hashikuchi's Lab" },
-    { name: "description", content: "Welcome to Hashikuchi's Lab!" },
-  ];
-}
-
 export default function Home() {
   return <Top />;
 }
